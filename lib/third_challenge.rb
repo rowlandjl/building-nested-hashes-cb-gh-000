1deb9788c002e779441cadfbc8eb640cc4b2cb74
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-20 13:22:55 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,10 @@ def third_challenge
       :heroine => {
         name: "Juliet", age: "15", status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
    }
   }
 
