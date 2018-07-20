d8be2f18a8eb65e2320d4092da787285cd611b69
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-20 13:23:22 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,10 @@ def third_challenge
       :hero => {
         name: "Romeo", age: "15", status: "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
    },
    :capulet => {
       :patriarch => {
@@ -23,8 +26,7 @@ def third_challenge
         name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+
       ]
    }
   }
